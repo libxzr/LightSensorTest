ef4acd7ecee4a18f73b2e2e233b88bc19e0e63f3
LibXZR <i@xzr.moe>
2022-06-24 16:15:15 +0800

    Support long press to change light source color

--- app/src/main/java/moe/xzr/lightsensortest/UDSensorTestActivity.kt
+++ app/src/main/java/moe/xzr/lightsensortest/UDSensorTestActivity.kt
@@ -1,20 +1,22 @@
 package moe.xzr.lightsensortest
 
 import android.app.Activity
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewTreeObserver
-import android.view.WindowInsets
+import android.view.*
 import android.widget.SeekBar
 import android.widget.TextView
 
 class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, SensorEventListener,
-    ViewTreeObserver.OnDrawListener {
+    ViewTreeObserver.OnDrawListener, GestureDetector.OnGestureListener {
+    companion object {
+        private val LIGHT_COLORS = listOf("#ffffff", "#ff0000", "#00ff00", "#0000ff")
+    }
+
     private lateinit var sensorManager: SensorManager
     private lateinit var lightSensor: Sensor
 
@@ -24,6 +26,10 @@ class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, Sensor
     private lateinit var posText: TextView
     private lateinit var lightText: TextView
     private lateinit var brightnessText: TextView
+
+    private lateinit var gestureDetector: GestureDetector
+
+    private var lightSourceColorIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +46,8 @@ class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, Sensor
         posText = findViewById(R.id.pos_text)
         lightText = findViewById(R.id.light_text)
         brightnessText = findViewById(R.id.brightness_text)
+
+        gestureDetector = GestureDetector(this, this)
 
         sizeChanger.setOnSeekBarChangeListener(this)
         brightnessChanger.setOnSeekBarChangeListener(this)
@@ -58,13 +66,15 @@ class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, Sensor
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (gestureDetector.onTouchEvent(event))
+            return true
         var x = event?.x ?: 0f
         var y = event?.y ?: 0f
         x -= lightSource.width / 2 * lightSource.scaleX
         y -= lightSource.height / 2 * lightSource.scaleY
         lightSource.translationX = x
         lightSource.translationY = y
-        return true;
+        return true
     }
 
     private fun updateBrightness(brightness: Float) {
@@ -87,6 +97,11 @@ class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, Sensor
 
     private fun progressToScale(progress: Int): Float {
         return progress.toFloat() / 100
+    }
+
+    private fun updateLightColor() {
+        lightSourceColorIndex = ++lightSourceColorIndex % LIGHT_COLORS.size
+        lightSource.setBackgroundColor(Color.parseColor(LIGHT_COLORS[lightSourceColorIndex]))
     }
 
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -121,5 +136,39 @@ class UDSensorTestActivity : Activity(), SeekBar.OnSeekBarChangeListener, Sensor
 
     override fun onDraw() {
         updatePosText()
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        updateLightColor()
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return false
     }
 }
